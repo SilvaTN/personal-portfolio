de4b5a9fb7a46b6345ee18cf694d3ae89e5c18cf
tales <talesdenorie@gmail.com>
2021-01-23 11:37:15 +1300

    added letterByLetter print

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,49 @@
 
 // this tutorial helped https://www.youtube.com/watch?v=XQEfWd1lh4Q&t=303s
+// show txt letter by letter https://stackoverflow.com/questions/7264974/show-text-letter-by-letter
+// jquery for beginners https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery  and  https://www.w3schools.com/jquery/jquery_get_started.asp 
 
+
+var hireButton = document.getElementById("hire-button");
 var button = document.getElementById('funny-button');
 var numDisplay = document.getElementById("num-display")
 var timesClicked = 0;
 
 var timeDisplay = document.getElementById("time-display")
+var letterByLetterDisplay = document.getElementById("letter-by-letter-display")
+
+
+var txtPrintInProgress = false;
+
+
+
+hireButton.onclick = function() {
+    //make the button not be able to be pressed until the whole message is printed, i.e. when clearinterval is called.
+
+    if (!txtPrintInProgress) {
+        hireButton.innerHTML = "button works";
+
+        letterByLetterDisplay.innerHTML = "";
+        var message = 'Hello world';
+        var index = 0;
+        function displayLetter() {
+            if (index < message.length) {
+                letterByLetterDisplay.append(message[index++]);
+            }
+            else{
+                //clearInterval is only called after whole message has been printed.
+                clearInterval(repeat);
+                txtPrintInProgress = false;
+            }
+        }
+        txtPrintInProgress = true;
+        //The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+        var repeat = setInterval(displayLetter, 100);
+    }
+
+
+}
+
 
 button.onclick = function() {
 
@@ -32,3 +70,4 @@ button.onclick = function() {
     }
     
 }
+
